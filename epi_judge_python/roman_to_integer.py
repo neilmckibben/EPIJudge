010b7562b002c08c3f7c946e39ab665a9a3161ef
neilmckibben-osu <mckibben.64@osu.edu>
2019-08-20 13:45:50 -0400

    roman to int

--- epi_judge_python/roman_to_integer.py
+++ epi_judge_python/roman_to_integer.py
@@ -2,8 +2,23 @@ from test_framework import generic_test
 
 
 def roman_to_integer(s):
-    # TODO - you fill in here.
-    return 0
+    val = 0
+    keyDict = dict(I=1, V=5, X=10, L=50, C=100, D=500, M=1000)
+    length = len(s)-2
+    val += keyDict[s[-1]]
+    while length >= 0:
+        current = s[length]
+        currentInt = keyDict[current]
+        back = s[length+1]
+        backInt = keyDict[back]
+        if(backInt > currentInt):
+            val = val - currentInt
+        else:
+            val += currentInt
+        length = length-1
+
+    return val
+
 
 
 if __name__ == '__main__':
