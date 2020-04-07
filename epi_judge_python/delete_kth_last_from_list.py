9bc9b0cf6bbb6338962507fb3f0474c673f6ef43
neilmckibben <neilmckibben@gmail.com>
2020-04-06 20:28:38 -0700

    k-element rem

--- epi_judge_python/delete_kth_last_from_list.py
+++ epi_judge_python/delete_kth_last_from_list.py
@@ -3,16 +3,15 @@ from test_framework import generic_test
 
 # Assumes L has at least k nodes, deletes the k-th last node in L.
 def remove_kth_last(L, k):
-    head = L
-    super_front = L
-    for i in range(0, k):
-        super_front = super_front.next
-    prev = None
-    while super_front is not None:
-        prev = L
-        super_front = super_front.next
-        L = L.next
-    prev = L.next
+    front, back, head = L, L, L
+    for i in range(0, k + 1):
+        if not front:
+            return head.next
+        front = front.next
+    while front:
+        front = front.next
+        back = back.next
+    back.next = back.next.next
     return head
 
 
