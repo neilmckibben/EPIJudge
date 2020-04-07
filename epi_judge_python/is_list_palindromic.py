0bc59b9b44738c6b7e0b94091c20715c552002ac
neilmckibben <neilmckibben@gmail.com>
2020-04-07 14:17:40 -0700

    even_odd_list_merge.py

--- epi_judge_python/is_list_palindromic.py
+++ epi_judge_python/is_list_palindromic.py
@@ -2,7 +2,19 @@ from test_framework import generic_test
 
 
 def is_linked_list_a_palindrome(L):
-    # TODO - you fill in here.
+    head = L
+    length = 0
+    while head:
+        length += 1
+        head = head.next
+    front, back = L, L
+    middle = length // 2
+    for i in range(0, middle):
+        back = back.next
+    while back:
+        if front is not back:
+            return False
+        front, back = front.next, back.next
     return True
 
 
@@ -11,3 +23,4 @@ if __name__ == '__main__':
         generic_test.generic_test_main("is_list_palindromic.py",
                                        'is_list_palindromic.tsv',
                                        is_linked_list_a_palindrome))
+
