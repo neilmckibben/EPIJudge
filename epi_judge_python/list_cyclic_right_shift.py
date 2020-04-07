51888b9a55933ff6dc0c8593bccbb8f13675d86f
neilmckibben <neilmckibben@gmail.com>
2020-04-06 21:41:27 -0700

    list_cyclic_right_shift.py

--- epi_judge_python/list_cyclic_right_shift.py
+++ epi_judge_python/list_cyclic_right_shift.py
@@ -2,8 +2,23 @@ from test_framework import generic_test
 
 
 def cyclically_right_shift_list(L, k):
-    # TODO - you fill in here.
-    return None
+    front, back, i, new_front, length = L, L, L, None, 0
+    while i:
+        i = i.next
+        length += 1
+    if length < 2:
+        return L
+    shift = k % length
+    if shift == 0:
+        return L
+    for i in range(0, shift):
+        front = front.next
+    while front.next:
+        front, back = front.next, back.next
+    new_front = back.next
+    back.next = None
+    front.next = L
+    return new_front
 
 
 if __name__ == '__main__':
