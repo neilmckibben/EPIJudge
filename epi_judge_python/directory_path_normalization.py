d99d1b369d8539b027d4c21ee0175fd852b80195
neilmckibben <neilmckibben@gmail.com>
2020-04-08 21:24:09 -0700

    directory_path_normalization.py Attempted

--- epi_judge_python/directory_path_normalization.py
+++ epi_judge_python/directory_path_normalization.py
@@ -1,9 +1,24 @@
 from test_framework import generic_test
+import functools
 
 
 def shortest_equivalent_path(path):
-    # TODO - you fill in here.
-    return ''
+    path = path.split("/")
+    list_path = []
+    for movement in path:
+        if movement == "..":
+            if len(list_path) == 0 or list_path[-1] == "..":
+                list_path.append("..")
+            else:
+                list_path.pop()
+        elif movement != ".":
+            list_path.append(movement)
+
+    if list_path[-1] == "":
+        list_path = list_path[:-1]
+    answer_path = functools.reduce(lambda a, b: a + "/" + str(b), list_path)
+    
+    return answer_path
 
 
 if __name__ == '__main__':
