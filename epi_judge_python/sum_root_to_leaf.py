89a47181a9c182077492ec86856b239d56fbfc0b
neilmckibben <neilmckibben@gmail.com>
2020-04-11 14:01:52 -0700

    sum_root_to_leaf.py

--- epi_judge_python/sum_root_to_leaf.py
+++ epi_judge_python/sum_root_to_leaf.py
@@ -1,11 +1,26 @@
 from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
 
+def parse(tree: BinaryTreeNode, values: list, path: str):
+    if tree:
+        path += str(tree.data)
+        if tree.left or tree.right:
+            if tree.left:
+                parse(tree.left, values, path)
+            if tree.right:
+                parse(tree.right, values, path)
+        else:
+            values.append(path)
+
+
 
 def sum_root_to_leaf(tree: BinaryTreeNode) -> int:
-    # TODO - you fill in here.
-    return 0
-
+    values = list()
+    parse(tree, values, "")
+    sum = 0
+    for value in values:
+        sum += int(value, 2)
+    return sum
 
 if __name__ == '__main__':
     exit(
