9a877cea865b927cb1bf4da29074b26e80005a0e
neilmckibben <neilmckibben@gmail.com>
2020-04-10 20:23:38 -0700

    is_tree_symmetric.py

--- epi_judge_python/is_tree_symmetric.py
+++ epi_judge_python/is_tree_symmetric.py
@@ -1,8 +1,18 @@
 from test_framework import generic_test
 
+def parse(left, right):
+    if left is None and right is None:
+        return True
+    elif left is None or right is None:
+        return False
+    elif left.data != right.data:
+        return False
+
+    return parse(left.left, right.right) and parse(left.right, right.left)
 
 def is_symmetric(tree):
-    # TODO - you fill in here.
+    if tree:
+        return parse(tree.left, tree.right)
     return True
 
 
