35836b0545733e716d6779711a878590257b730e
neilmckibben <neilmckibben@gmail.com>
2020-04-10 20:40:30 -0700

    triv

--- epi_judge_python/advance_by_offsets.py
+++ epi_judge_python/advance_by_offsets.py
@@ -4,18 +4,6 @@ from test_framework import generic_test
 
 
 def can_reach_end(A):
-    if len(A) == 1:
-        return True
-    max_attainable = A[0]
-    i = 1
-    while i <= max_attainable:
-        if A[i] + i >= len(A) - 1:
-            return True
-        increase = (A[i] + i - max_attainable)
-        if increase > 0:
-            max_attainable += (A[i] + i - max_attainable)
-        i += 1
-    return False
 
 
 # def evaluate(A, start, length):
--- epi_judge_python/buy_and_sell_stock.py
+++ epi_judge_python/buy_and_sell_stock.py
@@ -1,7 +1,9 @@
+from typing import List
+
 from test_framework import generic_test
 
 
-def buy_and_sell_stock_once(prices):
+def buy_and_sell_stock_once(prices: List[float]) -> float:
     # TODO - you fill in here.
     # 2 pointer solution
     max_profit = 0
@@ -15,6 +17,6 @@ def buy_and_sell_stock_once(prices):
 
 if __name__ == '__main__':
     exit(
-        generic_test.generic_test_main("buy_and_sell_stock.py",
-                                       "buy_and_sell_stock.tsv",
+        generic_test.generic_test_main('buy_and_sell_stock.py',
+                                       'buy_and_sell_stock.tsv',
                                        buy_and_sell_stock_once))
