cd94a9dc89909d29ae204bc9244dbd422132f50c
neilmckibben <neilmckibben@gmail.com>
2020-04-14 22:53:20 -0700

    apply_permutation.py

--- epi_judge_python/apply_permutation.py
+++ epi_judge_python/apply_permutation.py
@@ -4,12 +4,10 @@ from test_framework import generic_test
 
 
 def apply_permutation(perm: List[int], A: List[int]) -> None:
-    answer = list()
     for i in range(0, len(perm)):
-        perm[i] = A[perm[i]]
-    # print(perm)
-    # print(A)
-    # print(len(perm), len(A))
+        while i != perm[i]:
+            A[i], A[perm[i]] = A[perm[i]], A[i]
+            perm[perm[i]], perm[i] = perm[i], perm[perm[i]]
     return
 
 
@@ -23,3 +21,5 @@ if __name__ == '__main__':
         generic_test.generic_test_main('apply_permutation.py',
                                        'apply_permutation.tsv',
                                        apply_permutation_wrapper))
+# [7, 2, 11, 10, 4, 1, 15, 3, 9, 17, 18, 16, 14, 5, 0, 8, 6, 12, 13]
+# [3, 2, 11, 7, 4, 1, 15, 3, 9, 17, 18, 16, 14, 5, 0, 8, 6, 12, 13]
