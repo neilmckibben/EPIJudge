a5812cf616fd92bb8cf0d8b9bd9b8b025f222fd7
Neil Mckibben <mckibben.64@osu.edu>
2020-05-13 00:16:36 -0700

    is_string_permutable_to_palindrome.py

--- epi_judge_python/is_string_permutable_to_palindrome.py
+++ epi_judge_python/is_string_permutable_to_palindrome.py
@@ -1,8 +1,19 @@
 from test_framework import generic_test
 
-
+#Palindrome conditions:
+# All numbers are even or all are even and one thing is odd
 def can_form_palindrome(s: str) -> bool:
-    # TODO - you fill in here.
+    table = dict()
+    for character in s:
+        table[character] = table.get(character, 0) + 1
+    odd_occurence = False
+    for value in table.values():
+        if value % 2 == 1:
+            if odd_occurence:
+                return False
+            else:
+                odd_occurence = True
+
     return True
 
 
