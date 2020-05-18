14935f964b0570200aa0287f2fd04bd3df8e34c3
Neil Mckibben <mckibben.64@osu.edu>
2020-05-18 11:49:02 -0700

    string_decompositions_into_dictionary_words.py

--- epi_judge_python/string_decompositions_into_dictionary_words.py
+++ epi_judge_python/string_decompositions_into_dictionary_words.py
@@ -1,11 +1,26 @@
 from typing import List
-
+import collections
 from test_framework import generic_test
 
 
 def find_all_substrings(s: str, words: List[str]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    indexes = []
+    if words:
+        chunking_size, i = len(words[0]), 0
+        while i < len(s):
+            temp_list = collections.Counter(words)
+            substring = s[i:i+chunking_size]
+            j = i
+            while substring in temp_list:
+                temp_list[substring] -= 1
+                if temp_list[substring] == 0:
+                    del temp_list[substring]
+                if len(temp_list) == 0:
+                    indexes.append(i)
+                j += chunking_size
+                substring = s[j:j + chunking_size]
+            i += 1
+    return indexes
 
 
 if __name__ == '__main__':
