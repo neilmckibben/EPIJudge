9d00bb17eb8270205a06bdd7516c390054d049dc
Neil Mckibben <mckibben.64@osu.edu>
2020-05-17 20:49:30 -0700

    nearest_repeated_entries.py

--- epi_judge_python/nearest_repeated_entries.py
+++ epi_judge_python/nearest_repeated_entries.py
@@ -4,8 +4,15 @@ from test_framework import generic_test
 
 
 def find_nearest_repetition(paragraph: List[str]) -> int:
-    # TODO - you fill in here.
-    return 0
+    mapping = dict()
+    closest_words = float('inf')
+    for i, word in enumerate(paragraph):
+        if word in mapping:
+            closest_words = min(i - mapping[word], closest_words)
+        mapping[word] = i
+    if closest_words == float('inf'):
+        closest_words = -1
+    return closest_words
 
 
 if __name__ == '__main__':
